0d514a032493c05b8f072ee0b22144398d714eed
vtspp <vtsppinho@gmail.com>
2020-03-30 12:32:10 -0300

    Inicio do projeto

---
+++ src/main/java/com/mongodb/MongoDbNoSqlApplication.java
@@ -0,0 +1,13 @@
+package com.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbNoSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbNoSqlApplication.class, args);
+	}
+
+}
